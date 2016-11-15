698379ababefff75fc5cbeb1aa828a99a8e70fe9
Eivind Lærum <eivind@bvnetwork.com>
2016-11-15 14:35:46 +0100

    New block type for registration #28

---
+++ src/BVNetwork.Attend/Models/Blocks/RegistrationBlock.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+
+namespace BVNetwork.Attend.Models.Blocks
+{
+    [ImageUrl("~/Modules/BVNetwork.Attend/Static/attend_blue.png")]
+    [ContentType(DisplayName = "Registration", GUID = "57482cfd-9943-49a1-b301-cc39efc90fc2", Description = "")]
+    public class RegistrationBlock : BlockData
+    {
+
+    }
+}
