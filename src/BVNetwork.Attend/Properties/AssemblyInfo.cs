29c048b0129e6427fa8c617d7bdb54faf33ddcae
Pontus Hagman <pontus@bvnetwork.com>
2017-09-08 10:49:24 +0200

    Updated nuget package folder

--- src/BVNetwork.Attend/Properties/AssemblyInfo.cs
+++ src/BVNetwork.Attend/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("BVNetwork.Attend")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("10.0.2.*")]
-[assembly: AssemblyFileVersion("10.0.2.*")]
+[assembly: AssemblyVersion("10.0.3.0")]
+[assembly: AssemblyFileVersion("10.0.3.0")]
